522298a858df72372c57b0079a7979341a07b9fa
hungndh <ndhhung1011@gmail.com>
2023-08-29 13:55:59 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import re
+from flask import Flask, request
+import telegram
+from app.config import bot_token, bot_user_name,URL
+
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+  # retrieve the message in JSON and then transform it to Telegram object
+  update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+  chat_id = update.message.chat.id
+  msg_id = update.message.message_id
+
+  # Telegram understands UTF-8, so encode text for unicode compatibility
+  text = update.message.text.encode('utf-8').decode()
+  # for debugging purposes only
+  print("got text message :", text)
+  # the first time you chat with the bot AKA the welcoming message
+  if text == "/start":
+      # print the welcoming message
+      bot_welcome = """
+      Welcome
+      """
+      # send the welcoming message
+      bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+  return 'ok'
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+  s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+  if s:
+      return "webhook setup ok"
+  else:
+      return "webhook setup failed"
+
+@app.route('/')
+def index():
+  return '.'
+
+
+if __name__ == '__main__':
+  app.run()
---
+++ app/config.py
@@ -0,0 +1,3 @@
+bot_token = "6315182691:AAEahpdlEdbWa4hC4UIPKp3JKuCfLraB1Xg"
+bot_user_name = "koen_news_bot"
+URL = "https://stark-shore-20043-ab75e9bd4a83.herokuapp.com"
